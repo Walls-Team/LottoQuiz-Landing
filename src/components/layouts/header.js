c01e9d88d14702cb583efb20e8279a276cbaaeda
Kevin Chacon <kevinchacon08@gmail.com>
2023-03-21 20:12:00 -0400

    add header

---
+++ src/components/layouts/header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Banners from '../../elements/banners'
+import '../css/header.scss'
+
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className='background'>
+        <img className='b1' src='./ball1.png'/>
+        <img className='b2' src='./ball2.png'/>
+        <img className='b3' src='./ball3.png'/>
+        <img className='b4' src='./ball4.png'/>
+        <img className='b5' src='./ball5.png'/>
+        <img className='b6' src='./ball6.png'/>
+        <img className='b7' src='./ball7.png'/>
+        <img className='b8' src='./ball8.png'/>
+      </div>
+      <div className='elipse'/>
+        <div className='cointainer-banners'>
+            <Banners/>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Header from '../components/layouts/header'
+import './css/home.scss'
 
 function Home() {
   return (
-    <div>home</div>
+    <>
+    <div className='container-header'>
+      <Header />
+    </div>
+    </>
   )
 }
 
