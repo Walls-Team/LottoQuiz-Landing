035256e476885eb2d2e18e1fc43bc032042cd300
CarlosD95 <carlosdaserrano4@gmail.com>
2023-04-07 20:58:13 -0400

    navbar and lotteryCard

--- src/components/lotteryCard.js
+++ src/components/lotteryCard.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/lotteryCard.scss';
 
 const LotteryCard = () => {
 
-  const dataLottery = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+  const [numbers, setNumbers] = useState([]);
+  
+  const addNumber = (number) => {
+    if(numbers.length < 9){
+      setNumbers([...numbers, number])
+    } 
+  }
 
+  const deletedList = () => {
+    setNumbers([]);
+  }
+
+  const randomList = () => {
+
+    let list = [];
+
+      for (let i = 0; i < 9; i++) {
+        let random = Math.random();
+        random = random * 25 + 1;
+        random = Math.trunc(random);
+        list[i] = random;
+      }
+
+    setNumbers(list);
+
+   /* var x = Math.ceil(Math.random()*25);
+    if (numbers.length < 9 ) {
+      setNumbers([...numbers, x])
+    } 
+   */
+  }
+  
   return (
     <div className="card">
         <div className="logo-card">
@@ -23,46 +53,57 @@ const LotteryCard = () => {
                     <span>
                         Elije tus 9 números o juega al
                     </span>
-                    <button className='btn1'>Azar</button>
-                    <button className='btn2'>Borrar</button>
+                    <button onClick={()=>randomList()}className='btn1'>Azar</button>
+                    <button onClick={()=>deletedList()} className='btn2'>Borrar</button>
+                </div>
+                <div className='ticket'>
+                  <span>{numbers[0]}</span>
+                  <span>{numbers[1]}</span>
+                  <span>{numbers[2]}</span>
+                  <span>{numbers[3]}</span>
+                  <span>{numbers[4]}</span>
+                  <span>{numbers[5]}</span>
+                  <span>{numbers[6]}</span>
+                  <span>{numbers[7]}</span>
+                  <span>{numbers[8]}</span>
                 </div>
             </div>
             <div className="lottery">
                 <table>
                     <tr>
-                      <td>{dataLottery[0]}</td>
-                      <td className='golden'>{dataLottery[1]}</td>
-                      <td >{dataLottery[2]}</td>
-                      <td className='golden'>{dataLottery[3]}</td>
-                      <td>{dataLottery[4]}</td>
+                      <td onClick={()=>addNumber("01")}>1</td>
+                      <td onClick={()=>addNumber("02")}>2</td>
+                      <td onClick={()=>addNumber("03")}>3</td>
+                      <td onClick={()=>addNumber("04")}>4</td>
+                      <td onClick={()=>addNumber("05")}>5</td>
                     </tr>
                     <tr>
-                      <td>{dataLottery[5]}</td>
-                      <td>{dataLottery[6]}</td>
-                      <td className='golden'>{dataLottery[7]}</td>
-                      <td>{dataLottery[8]}</td>
-                      <td>{dataLottery[9]}</td>
+                      <td onClick={()=>addNumber("06")}>6</td>
+                      <td onClick={()=>addNumber("07")}>7</td>
+                      <td onClick={()=>addNumber("08")}>8</td>
+                      <td onClick={()=>addNumber("09")}>9</td>
+                      <td onClick={()=>addNumber("10")}>10</td>
                     </tr>
                     <tr>
-                      <td className='golden'>{dataLottery[10]}</td>
-                      <td className='golden'>{dataLottery[11]}</td>
-                      <td>{dataLottery[12]}</td>
-                      <td>{dataLottery[13]}</td>
-                      <td className='golden'>{dataLottery[14]}</td>
+                      <td onClick={()=>addNumber("11")}>11</td>
+                      <td onClick={()=>addNumber("12")}>12</td>
+                      <td onClick={()=>addNumber("13")}>13</td>
+                      <td onClick={()=>addNumber("14")}>14</td>
+                      <td onClick={()=>addNumber("15")}>15</td>
                     </tr>
                     <tr>
-                      <td>{dataLottery[15]}</td>
-                      <td>{dataLottery[16]}</td>
-                      <td className='golden'>{dataLottery[17]}</td>
-                      <td>{dataLottery[18]}</td>
-                      <td>{dataLottery[19]}</td>
+                      <td onClick={()=>addNumber("16")}>16</td>
+                      <td onClick={()=>addNumber("17")}>17</td>
+                      <td onClick={()=>addNumber("18")}>18</td>
+                      <td onClick={()=>addNumber("19")}>19</td>
+                      <td onClick={()=>addNumber("20")}>20</td>
                     </tr>
                     <tr>
-                      <td className='golden'>{dataLottery[20]}</td>
-                      <td>{dataLottery[21]}</td>
-                      <td>{dataLottery[22]}</td>
-                      <td>{dataLottery[23]}</td>
-                      <td className='golden'>{dataLottery[24]}</td>
+                      <td onClick={()=>addNumber("21")}>21</td>
+                      <td onClick={()=>addNumber("22")}>22</td>
+                      <td onClick={()=>addNumber("23")}>23</td>
+                      <td onClick={()=>addNumber("24")}>24</td>
+                      <td onClick={()=>addNumber("25")}>25</td>
                     </tr>
                 </table>
             </div>
